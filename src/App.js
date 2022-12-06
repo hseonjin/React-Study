2414b748662524f02c9548570fc8f763fa17ce15
Heoseonjin <seonjin0524@naver.com>
2022-12-06 18:00:17 +0900

    day1: first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
+import { Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
+
+// -----------------------
+// 1-2장
+
+// JSX code
+// function Keyword -> create component 'App'
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   const name = "리액트";
+//   return (
+//     <>
+//       <h1>{name} 안녕!</h1>
+//       <h2>작동</h2>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// 조건부 연산자 &&: false인 경우 null과 같은 결과
+// function App() {
+//   const name = '리액트';
+
+//   return (
+//     <div>
+//       {name === '리액트' &&
+//         <h1>리액트입니다.</h1>}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 인라인 스타일링
+// function App() {
+//   const name = '리액트';
+//   const style = { backgroundColor: 'black',
+//                   color : 'aqua',
+//                   fontSize : '48px',
+//                   fontWeight : 'bold',
+//                   padding : 16  };
+
+//   return <div style = {style}>{name}</div>;
+// }
+
+// export default App;
+
+// css file -> className 지정
+// function App() {
+//   const name = '리액트';
+
+//   return <div className="react">{name}</div>;
+// }
+
+// export default App;
+
+// class형 component
+// class App extends Comment {
+//   render() {
+//     const name = 'react';
+//     return <div className="react">{name}</div>;
+//   }
+// }
---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// 함수형 컴포넌트
+// 화살표 함수 문법: 함수를 파라미터로 전달할 때 유용
+const MyComponent = () => {
+  return <div>나의 컴포넌트</div>;
+};
+
+export default MyComponent;
