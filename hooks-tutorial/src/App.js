3b56f89dbf7b2620ef48a145f2c962873c8196a5
Heoseonjin <seonjin0524@naver.com>
2022-12-13 18:38:39 +0900

    remove .git files

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Counter from "./Counter";
+import Info from "./info";
+import Average from "./Average";
+
+// const App = () => {
+//   return <Info />;
+// };
+
+const App = () => {
+  return <Average />;
+};
+
+export default App;
---
+++ hooks-tutorial/src/info.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+// const Info = () => {
+//   const [name, setName] = useState("");
+//   const [nickname, setNickname] = useState("");
+
+//   useEffect(() => {
+//     // console.log("렌더링 완료");
+//     // console.log({ name, nickname });
+//     // });
+
+//     // 마운팅될 때만 실행하는 경우
+//     // console.log("마운트될 때만 실행");
+//     // }, []);
+
+//     // 특정 값(name)이 업데이트될 때만 실행하는 경우
+//     // console.log(name);
+//     // }, [name]);
+
+//     // 뒷정리 함수
+//     console.log("effect");
+//     console.log(name);
+//     return () => {
+//       console.log("cleanup");
+//       console.log(name);
+//     };
+//   });
+
+//   const onChangeName = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   const onChangeNickname = (e) => {
+//     setNickname(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input value={name} onChange={onChangeName} />
+//         <input value={nickname} onChange={onChangeNickname} />
+//       </div>
+//       <div>
+//         <div>
+//           <b>이름: </b> {name}
+//         </div>
+//         <div>
+//           <b>닉네임: </b> {nickname}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Info;
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <b>이름: </b> {name}
+      </div>
+      <div>
+        <b>닉네임: </b> {nickname}
+      </div>
+    </div>
+  );
+};
+
+export default Info;
