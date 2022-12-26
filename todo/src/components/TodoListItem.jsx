955e60ca335b4c725300364b2db563cc25efd527
Heoseonjin <seonjin0524@naver.com>
2022-12-26 23:21:23 +0900

    test

--- todo/src/components/TodoListItem.jsx
+++ todo/src/components/TodoListItem.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
   MdRemoveCircleOutline,
-} from 'react-icons/md';
-import './TodoListItem.scss';
-import cn from 'classnames';
+} from "react-icons/md";
+import "./TodoListItem.scss";
+import cn from "classnames";
 
 const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+      <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
@@ -22,4 +22,4 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
